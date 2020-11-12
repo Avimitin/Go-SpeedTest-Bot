2f467f7fbaf2bcdffcd54954456242cad49fc1e7
Avimitin <avimitin@gmail.com>
2020-11-12 22:06:34 +0800

    Create post method

--- internal/speedtest/web/poster.go
+++ internal/speedtest/web/poster.go
@@ -1,5 +1,35 @@
 package web
 
-func Post(url string) {
-	//TODO: Send POST request
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	urlPak "net/url"
+	"strings"
+)
+
+// Post will make a post request with the url and data given by user
+func Post(url string, data map[string][]string) ([]byte, error) {
+	c := GetClient(30)
+	d := urlPak.Values{}
+	for k, v := range data {
+		d[k] = v
+	}
+	req, err := http.NewRequest("POST", url, strings.NewReader(d.Encode()))
+	if err != nil {
+		log.Printf("[WebPostError]%v", err)
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Printf("[WebPostError]%v", err)
+		return nil, err
+	}
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("[WebPostError]%v", err)
+		return nil, err
+	}
+	return content, nil
 }
