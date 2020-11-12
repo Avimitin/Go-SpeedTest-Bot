c0fe8956f6cb504fe3be0bec76b0974d1f14857e
Avimitin <avimitin@gmail.com>
2020-11-12 22:06:21 +0800

    Create Get method

--- internal/speedtest/web/getter.go
+++ internal/speedtest/web/getter.go
@@ -1,5 +1,25 @@
 package web
 
-func Get(url string) {
-	//TODO: Send GET request
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// Get method will make a get request with the url given by user.
+func Get(url string) ([]byte, error) {
+	if !isLegalURL(url) {
+		return nil, &illegalUrl{"URL missing prefix."}
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("[WebGetError]%v", err)
+		return nil, err
+	}
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("[WebGetError]%v", err)
+		return nil, err
+	}
+	return content, nil
 }
