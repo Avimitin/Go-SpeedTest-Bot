e571f8de47e173a0db6a51c70bbbbd31fca1c10e
Avimitin <avimitin@gmail.com>
2020-11-14 23:16:57 +0800

    Add all basic function test

--- internal/speedtest/speedtest_test.go
+++ internal/speedtest/speedtest_test.go
@@ -15,3 +15,35 @@ func TestGetHost(t *testing.T) {
 func TestPing(t *testing.T) {
 	fmt.Println(Ping(GetHost()))
 }
+
+func TestGetStatus(t *testing.T) {
+	fmt.Println(GetStatus(GetHost()))
+}
+
+func TestReadSubscriptions(t *testing.T) {
+	resp, err := ReadSubscriptions(GetHost(), "")
+	if err != nil {
+		t.Failed()
+	}
+	for _, r := range resp {
+		fmt.Println(r.Type, r.Config)
+	}
+}
+
+func TestGetResult(t *testing.T) {
+	resp, err := GetResult(GetHost())
+	if err != nil {
+		t.Failed()
+	}
+	fmt.Println(resp)
+}
+
+func TestStartTest(t *testing.T) {
+	subs, err := ReadSubscriptions(GetHost(), "")
+	if err != nil {
+		t.Failed()
+	}
+	sCFG := NewStartConfigs("ST_ASYNC", "TCP_PING", subs)
+	sCFG.Group = ""
+	StartTest(GetHost(), sCFG)
+}
