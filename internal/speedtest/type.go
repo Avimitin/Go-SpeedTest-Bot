ddb29eace937e8ad54dfecf800abc3d2648fba94
Avimitin <avimitin@gmail.com>
2020-11-14 23:18:33 +0800

    Add a host method for parsing host URL; add status, subsctions, node config, result, shadowconfig, resultinfo, startconfigs struct; add NewStartConfig method for get new startconfigs

--- internal/speedtest/type.go
+++ internal/speedtest/type.go
@@ -1,12 +1,81 @@
 package speedtest
 
+import "fmt"
+
 type Host struct {
 	IP    string
 	Token string
 	Port  int
 }
 
+func (h *Host) GetURL() string {
+	return fmt.Sprintf("http://%s:%d", h.IP, h.Port)
+}
+
 type Version struct {
 	Main   string `json:"main"`
 	WebAPI string `json:"webapi"`
+}
+
+type Status struct {
+	State string
+}
+
+type SubscriptionResp struct {
+	Type   string        `json:"type"`
+	Config *ShadowConfig `json:"config"`
+	Error  string        `json:"-"`
+}
+
+type ShadowConfig struct {
+	Server        string `json:"server"`
+	ServerPort    int    `json:"server_port"`
+	Method        string `json:"method"`
+	Protocol      string `json:"protocol"`
+	Obfs          string `json:"obfs"`
+	Plugin        string `json:"plugin"`
+	Password      string `json:"password"`
+	ProtocolParam string `json:"protocol_param"`
+	ObfsParam     string `json:"obfsparam"`
+	PluginOpts    string `json:"plugin_opts"`
+	PluginArgs    string `json:"plugin_args"`
+	Remarks       string `json:"remarks"`
+	Group         string `json:"group"`
+	Timeout       int    `json:"timeout"`
+	LocalPort     int    `json:"local_port"`
+	LocalAddress  string `json:"local_address"`
+	Fastopen      bool   `json:"fastopen"`
+	Obfsparam     string `json:"obfs_param"`
+}
+
+type Result struct {
+	Status  string       `json:"status"`
+	Current ShadowConfig `json:"current"`
+	Result  []ResultInfo `json:"results"`
+}
+
+type ResultInfo struct {
+	Group   string  `json:"group"`
+	Remarks string  `json:"remarks"`
+	Loss    float64 `json:"loss"`
+	Ping    float64 `json:"ping"`
+	GPing   float64 `json:"gPing"`
+}
+
+type StartConfigs struct {
+	TestMethod   string              `json:"testMethod"`
+	TestMode     string              `json:"testMode"`
+	Colors       string              `json:"colors"`
+	SortMethod   string              `json:"sortMethod"`
+	UseSSRCSharp bool                `json:"useSsrcSharp"`
+	Group        string              `json:"group"`
+	Configs      []*SubscriptionResp `json:"configs"`
+}
+
+func NewStartConfigs(testMethod string, testMode string, configs []*SubscriptionResp) *StartConfigs {
+	return &StartConfigs{
+		TestMethod: testMethod,
+		TestMode:   testMode,
+		Configs:    configs,
+	}
 }
