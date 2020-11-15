c9fde42769646379c2cf45715152d6acb726b7fb
avimitin <avimitin@gmail.com>
2020-11-15 16:19:24 +0800

    add subs file fetcher

--- internal/config/config.go
+++ internal/config/config.go
@@ -56,7 +56,23 @@ func GetBotFile() *ini.File {
 	if err != nil {
 		cfg, err = ini.Load(fmt.Sprintf("%s/config/bot.ini", os.Getenv("SPT_BOT_PATH")))
 		if err != nil {
-			log.Printf("[ConfigError]Unable to read host.ini: %v", err)
+			log.Printf("[ConfigError]Unable to read subs.ini: %v", err)
+			os.Exit(1)
+		}
+	}
+	return cfg
+}
+
+func GetSubsFile() *ini.File {
+	if !CFGExistTest() {
+		log.Printf("[ConfigError]No enough config")
+		os.Exit(1)
+	}
+	cfg, err := ini.Load("./config/subs.ini")
+	if err != nil {
+		cfg, err = ini.Load(fmt.Sprintf("%s/config/subs.ini", os.Getenv("SPT_BOT_PATH")))
+		if err != nil {
+			log.Printf("[ConfigError]Unable to read subs.ini: %v", err)
 			os.Exit(1)
 		}
 	}
