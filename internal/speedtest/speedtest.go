9cbdedc30c0c052b53d774f36871a54545d6dfb9
Avimitin <avimitin@gmail.com>
2020-11-18 22:22:45 +0800

    Redesign start test to goroutine

--- internal/speedtest/speedtest.go
+++ internal/speedtest/speedtest.go
@@ -56,17 +56,31 @@ func GetResult(h *Host) (*Result, error) {
 }
 
 // StartTest post a speed test request with the given node config.
-// This method won't return any response, before start a test user should call GetStatus method to fetch newest status.
-func StartTest(h *Host, startCFG *StartConfigs) {
+// Because of the blocking backend, This function is designed to be called as goroutine.
+// If error happen function will pass error message into status chan.
+// If state is not null it will pass status: ["running" / "done"].
+// If you are calling this method without authorized of given unexpected config it will pass error message.
+func StartTest(h *Host, startCFG *StartConfigs, status chan string) {
 	d, err := json.Marshal(startCFG)
 	if err != nil {
 		log.Println("[JSONError]Unable to marshall data", err)
 		return
 	}
 	fmt.Println(string(d))
-	_, err = web.JSONPost(h.GetURL()+"/start", d)
+	resp, err := web.JSONPostWithTimeout(h.GetURL()+"/start", d, 0)
 	if err != nil {
 		log.Println("[WebGetError]Unable to connect to backend")
 		return
+	}
+	var state Status
+	err = json.Unmarshal(resp, &state)
+	if err != nil {
+		status <- err.Error()
+		return
+	}
+	if state.State != "" {
+		status <- state.State
+	} else {
+		status <- state.Error
 	}
 }
