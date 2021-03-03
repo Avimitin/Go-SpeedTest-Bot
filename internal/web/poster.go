238c05980824c76d1a04d4592e49781b82f1e081
avimitin <avimitin@avimitin-chb.localdomain>
2021-03-03 14:40:26 +0800

    deleted useless logging and return useful error for JSONPost

--- internal/web/poster.go
+++ internal/web/poster.go
@@ -2,11 +2,12 @@ package web
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	urlPak "net/url"
+	"time"
 )
 
 // Post will make a post request with the url and data given by user
@@ -28,23 +29,27 @@ func JSONPost(url string, data []byte) ([]byte, error) {
 }
 
 // JSONPostWithTimeout will post a JSON request with the url and data given by user, it will return error if reach time limit.
-func JSONPostWithTimeout(url string, data []byte, timeout int) ([]byte, error) {
-	c := GetClient(timeout)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+func JSONPostWithTimeout(url string, data []byte, timeout time.Duration) ([]byte, error) {
+	var ctx context.Context
+	if timeout == 0 {
+		ctx = context.Background()
+	} else {
+		var cancel context.CancelFunc
+		ctx, cancel = context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewBuffer(data))
 	if err != nil {
-		log.Printf("[WebPostError]%v", err)
-		return nil, err
+		return nil, fmt.Errorf("new request %s: %v", url, err)
 	}
 	req.Header.Set("content-type", "application/json")
-	resp, err := c.Do(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Printf("[WebPostError]%v", err)
-		return nil, err
+		return nil, fmt.Errorf("send request %s: %v", url, err)
 	}
 	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Printf("[WebPostError]%v", err)
-		return nil, err
+		return nil, fmt.Errorf("read response from %s: %v", url, err)
 	}
 	return content, nil
 }
