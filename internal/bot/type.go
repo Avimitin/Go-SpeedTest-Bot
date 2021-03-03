82afa6ae2b216d2e24a2c95533c209c842d73b86
avimitin <avimitin@avimitin-chb.localdomain>
2021-03-03 20:01:32 +0800

    deleted Conf type

--- internal/bot/type.go
+++ internal/bot/type.go
@@ -2,11 +2,6 @@ package bot
 
 import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 
-// BotConf store all bot config
-type Conf struct {
-	token string
-}
-
 type B = tgbotapi.BotAPI
 type M = tgbotapi.Message
 
