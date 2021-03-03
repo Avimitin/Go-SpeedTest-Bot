525823b8bcdf1fcee42c7a415da2a9ef4296d195
avimitin <avimitin@avimitin-chb.localdomain>
2021-03-03 21:56:46 +0800

    added config.json operation in config.go

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,29 +1,34 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
-
-	"gopkg.in/ini.v1"
 )
 
 func findConfigFilePath() string {
 	var route string
 	if route = os.Getenv("SPT_CFG_PATH"); route != "" {
-		return path.Join(route, "config.ini")
+		return path.Join(route, "config.json")
 	}
 	if route = os.Getenv("HOME"); route != "" {
-		return path.Join(route, ".config", "spt_bot", "config.ini")
+		return path.Join(route, ".config", "spt_bot", "config.json")
 	}
-	return path.Join(".", "config", "config.ini")
+	return path.Join(".", "config", "config.json")
 }
 
-func GetConfig() (*ini.File, error) {
+func GetConfig() (*Configuration, error) {
 	path := findConfigFilePath()
-	cfg, err := ini.Load(path)
+	configFile, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, fmt.Errorf("open %s: %v", path, err)
+		return nil, fmt.Errorf("read %q: %v", path, err)
+	}
+	var cfg *Configuration
+	err = json.Unmarshal(configFile, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("parse file: %v", err)
 	}
 	return cfg, nil
 }
@@ -31,10 +36,9 @@ func GetConfig() (*ini.File, error) {
 func GetToken() string {
 	file, err := GetConfig()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		fatal(err)
 	}
-	return file.Section("Bot").Key("token").String()
+	return file.Global.Token
 }
 
 type HostConfig struct {
@@ -46,14 +50,33 @@ func (hc *HostConfig) GetURL() string {
 	return hc.address
 }
 
-func GetHost() *HostConfig {
+func GetRunner(runnername string) *Runner {
 	file, err := GetConfig()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		fatal(err)
 	}
-	return &HostConfig{
-		address: file.Section("Host").Key("address").String(),
-		key:     file.Section("Host").Key("key").String(),
+	for _, f := range file.Runner {
+		if f.Name == runnername {
+			return f
+		}
 	}
+	return nil
+}
+
+func GetDefaultConfig(configname string) *Default {
+	file, err := GetConfig()
+	if err != nil {
+		fatal(err)
+	}
+	for _, f := range file.DefaultConfig {
+		if f.Name == configname {
+			return f
+		}
+	}
+	return nil
+}
+
+func fatal(err error) {
+	fmt.Println(err)
+	os.Exit(1)
 }
