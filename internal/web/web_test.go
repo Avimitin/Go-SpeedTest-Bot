474e715907d0df8a496cfd897e2102a3c0439a2b
avimitin <avimitin@avimitin-chb.localdomain>
2021-03-03 15:18:55 +0800

    added test for web

--- internal/web/web_test.go
+++ internal/web/web_test.go
@@ -2,26 +2,62 @@ package web
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 	"testing"
 )
 
+const (
+	addr             = "http://127.0.0.1:8888"
+	testAmount int32 = 2
+)
+
+var (
+	sig = make(chan interface{})
+)
+
+func TestMain(m *testing.M) {
+	go func() {
+		m.Run()
+		var done int32
+		for range sig {
+			if done == testAmount {
+				os.Exit(0)
+			}
+		}
+	}()
+	http.ListenAndServe(addr, http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			fmt.Fprint(rw, "message")
+		} else if r.Method == http.MethodPost {
+			fmt.Fprint(rw, r.Body)
+		}
+	}))
+}
+
 func TestGet(t *testing.T) {
-	resp, err := Get("https://baidu.com")
+	get, err := Get(addr)
 	if err != nil {
-		fmt.Printf("%v", err)
-		t.Fail()
+		t.Fatal(err)
 	}
-	fmt.Printf("%s", string(resp))
+	want := "message"
+	if string(get) != want {
+		t.Errorf("get %s want %s", get, want)
+	}
+	sig <- 0
 }
 
 func TestPost(t *testing.T) {
 	data := map[string][]string{
-		"title":  {"foo", "bar"},
-		"userID": {"123"},
+		"title": {"foo"},
 	}
-	resp, err := Post("https://jsonplaceholder.typicode.com/posts", data)
+	get, err := Post(addr, data)
 	if err != nil {
-		t.Fail()
+		t.Fatal(err)
 	}
-	fmt.Printf("%s", string(resp))
+	want := `"title": "foo"`
+	if string(get) != want {
+		t.Errorf("get %s want %s", get, want)
+	}
+	sig <- 0
 }
