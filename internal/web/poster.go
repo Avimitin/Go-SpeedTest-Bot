458e52720c54f48e018551104eebb5ae43cc44dc
avimitin <avimitin@avimitin-chb.localdomain>
2021-03-03 14:26:45 +0800

    deleted useless params transition and code for post

--- internal/web/poster.go
+++ internal/web/poster.go
@@ -2,35 +2,22 @@ package web
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	urlPak "net/url"
-	"strings"
 )
 
 // Post will make a post request with the url and data given by user
-func Post(url string, data map[string][]string) ([]byte, error) {
-	c := GetClient(30)
-	d := urlPak.Values{}
-	for k, v := range data {
-		d[k] = v
-	}
-	req, err := http.NewRequest("POST", url, strings.NewReader(d.Encode()))
+func Post(url string, d urlPak.Values) ([]byte, error) {
+	resp, err := http.PostForm(url, d)
 	if err != nil {
-		log.Printf("[WebPostError]%v", err)
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	resp, err := c.Do(req)
-	if err != nil {
-		log.Printf("[WebPostError]%v", err)
-		return nil, err
+		return nil, fmt.Errorf("post %s: %v", url, err)
 	}
 	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Printf("[WebPostError]%v", err)
-		return nil, err
+		return nil, fmt.Errorf("response from %s: %v", url, err)
 	}
 	return content, nil
 }
