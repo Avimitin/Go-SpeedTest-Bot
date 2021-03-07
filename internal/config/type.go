c740c73c08145b184ea9a353762118bae794cf17
avimitin <avimitin@avimitin-chb.localdomain>
2021-03-07 20:45:10 +0800

    moved runner type to runner package; packaging more runner method

--- internal/config/type.go
+++ internal/config/type.go
@@ -1,32 +1,19 @@
 package config
 
+import "go-speedtest-bot/internal/runner"
+
 // Configuration contains bot, runner configuration
 // at run time; Also store user define default
 // subscription config
 type Configuration struct {
-	Global        Global     `json:"global"`
-	Runner        []*Runner  `json:"runner"`
-	DefaultConfig []*Default `json:"default_config"`
-}
-
-type Runner struct {
-	Name   string `json:"name"`
-	Host   Host   `json:"host"`
-	Admins []int  `json:"admins"`
+	Global        Global           `json:"global"`
+	Runner        []*runner.Runner `json:"runner"`
+	DefaultConfig []*Default       `json:"default_config"`
 }
 
 type Global struct {
 	Token string `json:"token"`
 	Admin int    `json:"admin"`
-}
-
-type Host struct {
-	Address string `json:"address"`
-	Key     string `json:"key"`
-}
-
-func (h *Host) GetURL() string {
-	return h.Address
 }
 
 type Default struct {
---
+++ internal/runner/runner.go
@@ -0,0 +1,49 @@
+package runner
+
+import "sync/atomic"
+
+type Runner struct {
+	Status int32  // Status store runner status at local
+	Name   string `json:"name"`
+	Host   *Host  `json:"host"`
+	Admins []int  `json:"admins"`
+}
+
+type Host struct {
+	Address string `json:"address"`
+	Key     string `json:"key"`
+}
+
+func (h *Host) GetURL() string {
+	return h.Address
+}
+
+const (
+	Pending = iota // 0 == Pending
+	Working        // 1 == Working
+)
+
+// IsPending return boolen value about runner is pending or not
+func (r *Runner) IsPending() bool {
+	return r.GetRunnerStatus() == Pending
+}
+
+// IsPending return boolen value about runner is working or not
+func (r *Runner) IsWorking() bool {
+	return r.GetRunnerStatus() == Working
+}
+
+// GetRunnerStatus return current status
+func (r *Runner) GetRunnerStatus() int32 {
+	return atomic.LoadInt32(&r.Status)
+}
+
+// HangUp changed runner status to pending
+func (r *Runner) HangUp() {
+	atomic.CompareAndSwapInt32(&r.Status, Working, Pending)
+}
+
+// Activate changed runner status to working
+func (r *Runner) Activate() {
+	atomic.CompareAndSwapInt32(&r.Status, Pending, Working)
+}
