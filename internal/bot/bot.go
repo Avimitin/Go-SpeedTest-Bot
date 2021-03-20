64aac818ed4b74a8545161c7c67ea3e1470c8e73
Avimitin <avimitin@gmail.com>
2021-03-20 20:13:33 +0800

    split speedtest control flow from bot to controller package

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -3,9 +3,9 @@ package bot
 import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"go-speedtest-bot/internal/ArgsParser"
-	"go-speedtest-bot/internal/config"
-	"go-speedtest-bot/internal/speedtest"
+	"go-speedtest-bot/module/config"
+	"go-speedtest-bot/module/controller"
+	"go-speedtest-bot/module/pastebin"
 	"log"
 	"os"
 	"strings"
@@ -27,7 +27,7 @@ func SendT(cid int64, text string) {
 	msg := tgbotapi.NewMessage(cid, text)
 	_, err := defBot.Send(msg)
 	if err != nil {
-		log.Println("send %q: %v", text[:10]+"...", err)
+		log.Printf("send %q: %v", text[:10]+"...", err)
 	}
 }
 
@@ -37,13 +37,20 @@ func SendP(cid int64, text string, format string) {
 	msg.ParseMode = format
 	_, err := defBot.Send(msg)
 	if err != nil {
-		log.Println("send %q: %v", text[:10]+"...", err)
+		log.Printf("send %q: %v", text[:10]+"...", err)
 	}
 }
 
-// SendTF send text with formated content
+// SendTF send text with formatted content
 func SendTF(cid int64, content string, args ...interface{}) {
 	SendT(cid, fmt.Sprintf(content, args))
+}
+
+// SendErr send error message to given chat id
+// In addition, it will print the error to log.
+func SendErr(cid int64, err error) {
+	log.Println(err)
+	SendT(cid, err.Error())
 }
 
 // Listen is the robot's main methods. Calling this method will
@@ -90,6 +97,14 @@ func CMDHandler(bot *B, msg *M) {
 	}
 }
 
+func getArgs(m *M) []string {
+	var strs = strings.Fields(m.Text)
+	if len(strs) < 2 {
+		return nil
+	}
+	return strs[1:]
+}
+
 // cmd /start
 func cmdStart(b *B, m *M) {
 	text := "Here is a bot who can help you manage all your proxy."
@@ -97,147 +112,100 @@ func cmdStart(b *B, m *M) {
 }
 
 // cmd /ping
-func cmdPing(b *B, m *M) {
-	runners := config.GetAllRunner()
-	for _, r := range runners {
-		connected := speedtest.Ping(*r)
-		var text string
-		if connected {
-			text = "Connect to " + r.Name + " successfully"
-		} else {
-			text = "Unable to connect to the " + r.Name
-		}
-		SendT(m.Chat.ID, text)
+func cmdPing(m *M) {
+	args := getArgs(m)
+
+	if args == nil {
+		SendT(m.Chat.ID, "usage: /ping <runner-name>")
+		return
 	}
+
+	err := controller.PingFromName(m.From.ID, args[0])
+	if err != nil {
+		SendErr(m.Chat.ID, err)
+		return
+	}
+	SendTF(m.Chat.ID, "%s connected successfully", args[0])
 }
 
 // cmd /status
-func cmdStatus(b *B, m *M) {
-	args := strings.Fields(m.Text)
-	if len(args) < 2 {
+func cmdStatus(m *M) {
+	args := getArgs(m)
+	if args == nil {
 		SendT(m.Chat.ID, "Usage: /status <runner-name>")
 		return
 	}
-	runner := config.GetRunner(args[1])
-	result, err := speedtest.GetStatus(*runner)
+	status, err := controller.GetStatus(m.From.ID, args[0])
 	if err != nil {
-		SendT(m.Chat.ID, fmt.Sprint(err))
+		SendErr(m.Chat.ID, err)
 		return
 	}
-	SendT(m.Chat.ID, "Status: "+result.State)
+	SendT(m.Chat.ID, status)
 }
 
-// cmd /read_sub
-func cmdReadSub(b *B, m *M) {
-	args := strings.Fields(m.Text)
-	if len(args) < 3 {
-		SendT(m.Chat.ID, "Usage:\n/read_sub <sub> <runner>")
-		return
-	}
-	url := args[1]
-	runnername := args[2]
-	runner := config.GetRunner(runnername)
-	subResps, err := speedtest.ReadSubscriptions(*runner, url)
-	if err != nil {
-		SendT(m.Chat.ID, fmt.Sprint(err))
-		return
-	}
-	var text string
-	for _, subResp := range subResps {
-		if subResp.Type != "" && subResp.Config != nil {
-			text += subResp.Type + " " + subResp.Config.Remarks + "\n"
-		}
-	}
+// cmd /listrunner
+func cmdListRunner(m *M) {
+	var text = controller.ListRunner(m.From.ID)
 	SendT(m.Chat.ID, text)
 }
 
-func formatResult(r *speedtest.Result) string {
-	if len(r.Result) == 0 {
-		return ""
+// cmd /read_sub
+func cmdReadSub(m *M) {
+	args := getArgs(m)
+	if args == nil || len(args) < 2 {
+		SendT(m.Chat.ID, "Usage: /read_sub <sub> <runner>")
+		return
 	}
-	var text string
-	text = "Recent result(ls=loss, lp=local ping, gp=google ping):\n"
-	text += "Status: " + r.Status + "\n"
-	if len(r.Current.Remarks) != 0 {
-		text += "Nodes being tested: " + r.Current.Remarks + "\n"
+	url := args[0]
+	name := args[1]
+	resp, err := controller.ReadSubscriptions(
+		m.From.ID,
+		url,
+		name,
+	)
+	if err != nil {
+		SendErr(m.Chat.ID, err)
+		return
 	}
-	text += "\n"
-	for _, res := range r.Result {
-		text += fmt.Sprintf(
-			"%s: | ls: %.2f%% | lp: %.2f ms | gp: %.2f ms\n", res.Remarks, res.Loss*100, res.Ping*1000, res.GPing*1000)
-	}
-	return text
+	SendT(m.Chat.ID, resp)
 }
 
 // cmd /result
-func cmdResult(b *B, m *M) {
-	args := strings.Fields(m.Text)
-	if len(args) < 2 {
+func cmdResult(m *M) {
+	args := getArgs(m)
+	if args == nil {
 		SendT(m.Chat.ID, "Usage: /result <runner-name>")
 		return
 	}
-	runner := config.GetRunner(args[1])
-	result, err := speedtest.GetResult(*runner)
+
+	text, err := controller.Result(m.From.ID, args[0])
 	if err != nil {
-		SendT(m.Chat.ID, fmt.Sprint(err))
+		SendErr(m.Chat.ID, err)
 		return
 	}
-	if fresult := formatResult(result); len(fresult) != 0 {
-		SendT(m.Chat.ID, fresult)
-		return
+
+	if config.PasteBinEnabled() {
+		resp, err := pastebin.PasteWithExpiry(
+			config.GetPasteBinKey(), "nodes test result", &text, "10m",
+		)
+		if err != nil {
+			SendErr(m.Chat.ID, err)
+			return
+		}
+		SendTF(m.Chat.ID, "result too long, view result at %s", resp)
 	}
-	SendT(m.Chat.ID, "No result yet")
+
+	SendT(m.Chat.ID, text)
 }
 
-func startTestWithURL(b *B, m *M, url string, method string, mode string, include []string, exclude []string) {
-	args := strings.Fields(m.Text)
-	if len(args) < 2 {
-		SendT(m.Chat.ID, "Usage: /run_url <runner-name>")
+// cmd /run
+func cmdStartTestWithURL(m *M) {
+	args := getArgs(m)
+	if args == nil {
+		SendT(m.Chat.ID, "Usage: /run <runner> url=<url> method=<method> mode=<mode>")
 		return
 	}
-	runner := config.GetRunner(args[1])
-	result, err := speedtest.GetStatus(*runner)
-	if err != nil {
-		SendT(m.Chat.ID, err.Error())
-		return
-	}
-	if result == nil {
-		SendT(m.Chat.ID, "Unable to fetch backend status, please try again later")
-		return
-	}
-	if result.State == "running" {
-		SendT(m.Chat.ID, "There is still a test running, please wait for all works done.")
-		return
-	}
-	nodes, err := speedtest.ReadSubscriptions(*runner, url)
-	if err != nil {
-		SendT(m.Chat.ID, err.Error())
-		return
-	}
-	if len(include) != 0 {
-		nodes = speedtest.IncludeRemarks(nodes, include)
-	}
-	if len(exclude) != 0 {
-		nodes = speedtest.ExcludeRemarks(nodes, exclude)
-	}
-	cfg := speedtest.NewStartConfigs(method, mode, nodes)
-	go speedtest.StartTest(*runner, cfg, make(chan string, 1))
-	SendT(m.Chat.ID, "Test started, you can use /result to check latest result.")
-}
-
-func parseMsgText(s string) map[string]string {
-	return ArgsParser.Parser(CfgFlags, s)
-}
-
-// cmd /run_url
-func cmdStartTestWithURL(b *B, m *M) {
-	if len(m.Text)-1 == len(m.Command()) || len(strings.Fields(m.Text)) < 3 {
-		SendP(m.Chat.ID, "Require subscriptions url.\n"+
-			"Use case: <code>/run_url -u https://example.com -M TCP_PING -m ST_ASYNC</code>\n(all in upper case)", "HTML")
-		return
-	}
-	args := parseMsgText(m.Text)
-	startTestWithURL(b, m, args["-u"], args["-m"], args["-M"], []string{}, []string{})
+	SendT(m.Chat.ID, "function under maintain")
 }
 
 // cmd /list_subs
@@ -253,21 +221,6 @@ func cmdListSubs(b *B, m *M) {
 		}
 	}
 	SendP(m.Chat.ID, text, "html")
-}
-
-// cmd /set_default
-func cmdSelectDefaultSub(b *B, m *M) {
-	if len(m.Text)-1 == len(m.Command()) || len(strings.Fields(m.Text)) < 2 {
-		SendT(m.Chat.ID, "Require one arguments. \nUse case: /set_default xxx")
-		return
-	}
-
-	defname := strings.Fields(m.Text)[1]
-	subsFile := config.GetDefaultConfig(defname)
-	if subsFile == nil {
-		SendT(m.Chat.ID, "Remarks not found.")
-		return
-	}
 }
 
 // cmd /schedule
