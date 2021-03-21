6449c736ffc1bf4ee343944fe7546e6ad37a87d5
avimitin <avimitin@gmail.com>
2021-03-21 11:31:17 +0000

    using no context request when timeout is zero value

--- module/web/poster.go
+++ module/web/poster.go
@@ -23,33 +23,39 @@ func Post(url string, d urlPak.Values) ([]byte, error) {
 	return content, nil
 }
 
-// JSONPost will post a JSON request with the url and data given by user. it will return error if timeout 30 seconds.
+// JSONPost will post a JSON request with the url and data given by user.
+// it will return error if timeout 30 seconds.
 func JSONPost(url string, data []byte) ([]byte, error) {
 	return JSONPostWithTimeout(url, data, 30)
 }
 
-// JSONPostWithTimeout will post a JSON request with the url and data given by user, it will return error if reach time limit.
+// JSONPostWithTimeout will post a JSON request with the url and data given by user,
+// it will return error if reach time limit.
 func JSONPostWithTimeout(url string, data []byte, timeout time.Duration) ([]byte, error) {
-	var ctx context.Context
+	var req *http.Request
+	var err error
 	if timeout == 0 {
-		ctx = context.Background()
+		req, err = http.NewRequest(http.MethodPost, url, bytes.NewBuffer(data))
 	} else {
-		var cancel context.CancelFunc
-		ctx, cancel = context.WithTimeout(context.Background(), timeout)
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
 		defer cancel()
+		req, err = http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewBuffer(data))
 	}
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewBuffer(data))
 	if err != nil {
 		return nil, fmt.Errorf("new request %s: %v", url, err)
 	}
+
 	req.Header.Set("content-type", "application/json")
+
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("send request %s: %v", url, err)
 	}
+
 	content, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("read response from %s: %v", url, err)
 	}
+
 	return content, nil
 }
